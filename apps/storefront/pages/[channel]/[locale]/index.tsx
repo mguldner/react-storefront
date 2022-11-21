94b279d439645b70acf7edff7e2d9c50b2620d97
Kerial <kerialdract@gmail.com>
2022-11-21 22:25:33 +0100

    Add slider main page

--- apps/storefront/pages/[channel]/[locale]/index.tsx
+++ apps/storefront/pages/[channel]/[locale]/index.tsx
@@ -1,6 +1,7 @@
 import { ApolloQueryResult } from "@apollo/client";
 import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import React, { ReactElement } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 
 import { HomepageBlock, Layout } from "@/components";
 import { BaseSeo } from "@/components/seo/BaseSeo";
@@ -37,14 +38,20 @@ function Home({ menuData }: InferGetStaticPropsType<typeof getStaticProps>) {
           <div className="container" />
         </header>
         <main>
-          <div className="container">
-            {menuData?.menu?.items?.map((m) => {
-              if (!m) {
-                return null;
-              }
-              return <HomepageBlock key={m.id} menuItem={m} />;
-            })}
-          </div>
+          <Splide aria-label="My Favorite Images">
+            <SplideSlide>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"
+                alt="Image 1"
+              />
+            </SplideSlide>
+            <SplideSlide>
+              <img
+                src="https://cdn-gulli.jnsmedia.fr/var/jeunesse/storage/images/gulli/chaine-tv/dessins-animes/pokemon/11153173-192-fre-FR/Pokemon.jpg"
+                alt="Image 2"
+              />
+            </SplideSlide>
+          </Splide>
         </main>
       </div>
     </>
