c44a0a3681ac2c38a36b015e4555108f620a4158
Kerial <mathieuguldner.emn@gmail.com>
2023-01-27 15:39:48 +0000

    Ajout image HomePage

--- apps/storefront/components/Layout/Layout.tsx
+++ apps/storefront/components/Layout/Layout.tsx
@@ -10,9 +10,14 @@ export function Layout({ children }: LayoutProps) {
   return (
     <>
       <SocialBanner />
-      <Navbar />
-      <div className="align-middle flex flex-col flex-grow">{children}</div>
-      <Footer />
+      <div className="bg-fixed bg-center bg-cover bg-[url('../public/image_bg.jpg')]">
+        <Navbar />
+        <div className="h-screen"></div>
+        <div className="bg-sky-800">
+          <div className="align-middle flex flex-col flex-grow">{children}</div>
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
