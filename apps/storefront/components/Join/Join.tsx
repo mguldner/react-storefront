41780ec7b2d9acd182748fc23dab8ba848e3bf8d
Kerial <mathieuguldner.emn@gmail.com>
2023-02-01 15:09:47 +0000

    Remplacement des apostrophes par '&apos;' afin de ne pas confondre dans les textes

--- apps/storefront/components/Join/Join.tsx
+++ apps/storefront/components/Join/Join.tsx
@@ -4,7 +4,7 @@ export function Join() {
   return (
     <div className="flex flex-col gap-10">
       <div className="text-white text-8xl text-center font-display">
-        {"Rejoins l'aventure et fais nous confiance comme d'autres dresseurs avant toi!"}
+        Rejoins l&apos;aventure et fais nous confiance comme d&apos;autres dresseurs avant toi!
       </div>
     </div>
   );
